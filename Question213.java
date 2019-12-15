4612bd2ae237eefef659fe6d14bd547963aa6a38
Bhanuprakash-b <54404090+Bhanuprakash-b@users.noreply.github.com>
2019-12-15 17:38:10 +0530

    Add files via upload

---
+++ Question213.java
@@ -0,0 +1,13 @@
+public class Question213{ 
+    public static void main(String[] args) { 
+    //Declare variable with name 'nptel', 'space' and 'java' and proper datatype.
+
+//Initialize the variables with proper input{
+	char a='2',b='8';
+String nptel=new String("NPTEL");
+String space=new String(" ");
+String java=new String("JAVA");
+System.out.println(a+space+b);
+System.out.print(nptel+space+java);
+   }
+}
